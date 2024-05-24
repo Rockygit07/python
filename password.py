dbe5832bb520672f400b8bd800e7bfb609407c84
Rockygit07 <166405698+Rockygit07@users.noreply.github.com>
2024-05-24 17:28:40 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,9 +1,13 @@
 import itertools
+from tqdm import tqdm
 
 def generate_wordlist(output_file, characters, word_length):
+    total_words = len(characters) ** word_length
     with open(output_file, 'w') as file:
-        for word in itertools.product(characters, repeat=word_length):
-            file.write(''.join(word) + '\n')
+        with tqdm(total=total_words) as pbar:
+            for word in itertools.product(characters, repeat=word_length):
+                file.write(''.join(word) + '\n')
+                pbar.update(1)
     print("Wordlist generated successfully!")
 
 
